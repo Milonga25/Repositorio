aaef4c39991c1405486037b3a83541cd8272621f
Milonga25 <kanthamoheba@hotmail.com>
2026-02-22 16:28:32 -0300

    .

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.expanduser('~/Repositorio')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Repositorio.base'
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
-
-application = get_wsgi_application()
